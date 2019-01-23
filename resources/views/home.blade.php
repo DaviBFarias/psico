266b8074233bec01ce82785f33ad6dc336ff4ecd
JadsonCris <jc.poledance@gmail.com>
2019-01-22 21:21:52 -0300

    Inicio do Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <title>HOME</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+    <div class="interface">
+        <header>
+            <nav>
+                <ul>
+                    <li><a>IDIOMA</a></li>
+                    <li><a>SAIR</a></li>
+                </ul>
+            </nav>
+        </header>
+        <header>
+            <nav>
+                <ul>
+                    <li><a>CADASTRO</a></li>
+                    
+                </ul>
+            </nav>
+        </header>
+    </div>
+</body>
+</html>
